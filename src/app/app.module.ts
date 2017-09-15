82963b37fff2b99792f8a3fce8274726ba35d383
Isuru Maduranga <isurumaldeniya.15@cse.mrt.ac.lk>
2017-09-15 17:59:37 +0530

    Add existing file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,27 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-
 import { AboutPage } from '../pages/about/about';
 import { RedditsPage } from '../pages/reddits/reddits';
 import { SettingsPage } from '../pages/settings/settings';
 import { TabsPage } from '../pages/tabs/tabs';
-
+import {HttpModule} from '@angular/http';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { DetailsPage } from '../pages/details/details';
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
     RedditsPage,
     SettingsPage,
+    DetailsPage,
     TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -29,7 +30,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     RedditsPage,
     SettingsPage,
-    TabsPage
+    TabsPage,
+    DetailsPage
   ],
   providers: [
     StatusBar,
